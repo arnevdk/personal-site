d8057bacd6f13fb8176c6ed272f46fce863bc3de
Arne Van Den Kerchove <arne@vandenkerchove.com>
2020-09-12 13:33:54 +0200

    added experiences and location

--- pages.js
+++ pages.js
@@ -1,13 +1,16 @@
 $('#page2').hide();
 
-var animationTime = 300;
+var animationTime = 200;
 $('#page-nav-1').click(function () {
-    $('#page1').hide("slide", { direction: "left" }, animationTime);
-    $('#page2').show("slide", { direction: "right" }, animationTime);
+    $('#page1').hide("slide", {direction: "left"}, animationTime)
+    $('#page1').promise().done(function () {
+        $('#page2').show("slide", {direction: "right"}, animationTime);
+    });
 });
 
 $('#page-nav-2').click(function () {
-    $('#page2').hide("slide", { direction: "right" }, animationTime);
-    $('#page1').show("slide", { direction: "left" }, animationTime);
-
+    $('#page2').hide("slide", {direction: "right"}, animationTime)
+    $('#page2').promise().done(function () {
+        $('#page1').show("slide", {direction: "left"}, animationTime);
+    });
 });
