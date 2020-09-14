60327abf805e9b1a447dc0650c82394a2447b582
Arne Van Den Kerchove <arne@vandenkerchove.com>
2020-09-14 12:15:10 +0200

    hastag links activated

--- pages.js
+++ pages.js
@@ -1,16 +1,34 @@
 $('#page2').hide();
 
 var animationTime = 200;
-$('#page-nav-1').click(function () {
+
+window.onhashchange = function () {
+    if (location.hash == '#experience')
+        pageExperience();
+    else
+        pageAboutMe();
+
+}
+
+function pageExperience() {
     $('#page1').hide("slide", {direction: "left"}, animationTime)
     $('#page1').promise().done(function () {
         $('#page2').show("slide", {direction: "right"}, animationTime);
     });
-});
+}
 
-$('#page-nav-2').click(function () {
+function pageAboutMe() {
     $('#page2').hide("slide", {direction: "right"}, animationTime)
     $('#page2').promise().done(function () {
         $('#page1').show("slide", {direction: "left"}, animationTime);
     });
+}
+
+
+$('#page-nav-1').click(function () {
+    pageExperience();
+});
+
+$('#page-nav-2').click(function () {
+    pageAboutMe();
 });
