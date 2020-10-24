d9cbf57a1a22fe88a368ebf8e889933f6e1e38be
Arne Van Den Kerchove <arne@vandenkerchove.com>
2020-10-24 21:58:23 +0200

    added loading screen

---
+++ loading.js
@@ -0,0 +1,15 @@
+window.addEventListener( 'load', onPageLoaded, false );
+window.addEventListener("pageshow", onPageShown, false);
+
+/* special event handler for ios Safari */
+function onPageShown(evt) {
+    // check if the page has been loaded from cache entirely
+    if (evt.persisted) {
+        // emulate the page loaded event
+        onPageLoaded();
+    }
+}
+
+function onPageLoaded() {
+    document.getElementById("loadingscreen").classList.add("hidden");
+}
